56f6c101c26d4acd04782d0a19ba0f00af5bab3e
Sebastiaan Berting <sebastiaan.berting@gmail.com>
2019-01-15 11:07:06 +0000

    app aangepast

--- application.py
+++ application.py
@@ -27,7 +27,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///likestack.db")
 
 
 @app.route("/")
