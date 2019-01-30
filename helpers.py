e777225051321589f8f58214ee92d2da192e9d90
YasBenAll <yassinbenallal@hotmail.com>
2019-01-30 19:15:58 +0000

    extra

--- helpers.py
+++ helpers.py
@@ -7,6 +7,7 @@ from flask import redirect, render_template, request, session
 from functools import wraps
 db = SQL("sqlite:///likestack.db")
 
+
 def apology(message, code=400):
     """Renders message as an apology to user."""
     return render_template("apology.html", top=code, bottom=message)
